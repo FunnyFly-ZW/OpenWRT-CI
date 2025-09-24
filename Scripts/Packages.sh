8979920ddc7788e0d5eb7fe48fdab0bb73aaddee
FunnyFly <zwchn007@gmail.com>
2025-09-25 06:52:17 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -78,7 +78,7 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 #find ./ | grep Makefile | grep mosdns | xargs rm -f
 #find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 #SmartDNS
 #UPDATE_PACKAGE "smartdns" "pymumu/openwrt-smartdns" "master"
